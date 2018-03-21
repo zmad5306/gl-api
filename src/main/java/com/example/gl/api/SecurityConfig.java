a462f0ba4a391cf221602de012c221629e2c5481
zmad5306 <zachary.maddox@gmail.com>
2018-03-21 11:30:19 -0400

    fixed password encoder

--- src/main/java/com/example/gl/api/SecurityConfig.java
+++ src/main/java/com/example/gl/api/SecurityConfig.java
@@ -4,13 +4,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
@@ -18,23 +17,22 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
-	protected UserDetailsService userDetailsService() {
-		UserDetails user1 = 
-				 User.withDefaultPasswordEncoder()
-	                .username("bob")
-	                .password("password")
-	                .roles("USER")
-	                .build();
-		
-		UserDetails user2 = 
-				 User.withDefaultPasswordEncoder()
-	                .username("sue")
-	                .password("password")
-	                .roles("USER")
-	                .build();
-		
-		return new InMemoryUserDetailsManager(user1, user2);
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("bob")
+				.password(passwordEncoder().encode("password"))
+				.roles("USER")
+				.and()
+			.withUser("sue")
+				.password(passwordEncoder().encode("password"))
+				.roles("USER")
+			.and()
+				.passwordEncoder(passwordEncoder());
 	}
 	
 	@Override
