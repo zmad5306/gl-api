3bfad104ee0531b1c22b4295cc0d0452a42af443
zmad5306 <zachary.maddox@gmail.com>
2018-03-23 16:22:55 -0400

    fixed health checks

--- src/main/java/com/example/gl/api/SecurityConfig.java
+++ src/main/java/com/example/gl/api/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/").permitAll()
+				.antMatchers("/", "/healthz").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
