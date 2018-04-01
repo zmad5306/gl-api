b2a0ebd2fa586d927f2a015feeac1e24baf27e2f
zmad5306 <zachary.maddox@gmail.com>
2018-03-31 22:12:47 -0400

    fixing docker build

--- src/test/java/com/example/gl/api/ApiApplicationTests.java
+++ src/test/java/com/example/gl/api/ApiApplicationTests.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
